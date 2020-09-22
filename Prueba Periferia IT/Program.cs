8c4d4178c758823486e11bfe9493928ed0c4ec2c
wilmilcard <leonjuandavid@hotmail.com>
2020-09-21 21:36:54 -0500

    Punto B

--- Prueba Periferia IT/Program.cs
+++ Prueba Periferia IT/Program.cs
@@ -17,7 +17,7 @@ namespace Prueba_Periferia_IT
                     punto_A();
                     break;
                 case 2:
-                    //punto_B();
+                    punto_B();
                     break;
                 case 3:
                     Console.WriteLine("Escogio 3");
@@ -55,6 +55,28 @@ namespace Prueba_Periferia_IT
             }
         }
 
-        
+        private static void punto_B()
+        {
+            //b. Escriba un algoritmo para calcular e imprimir los primeros 50 números primos
+
+            int a = 0, num = 1;
+
+            Console.WriteLine("Lista Numeros Primos");
+            while (num <= 50)
+            {
+                for (int i = 1; i < (num + 1); i++)
+                {
+                    if (num % i == 0)
+                    {
+                        a++;
+                    }
+                }
+                if (a == 2)
+                    Console.WriteLine($"{num}");
+                a = 0;
+                num += 1;
+            }
+        }
+
     }
 }
